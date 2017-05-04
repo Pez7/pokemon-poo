63bf283b1b5275e47e6fdd89348a9d5523dc26e7
Maria Paz <maria.pazrojasc@gmail.com>
2017-05-04 11:30:29 -0300

    ejercicio guiado

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+function Pokemon(nombre,color,poderDeAtaque){
+	this.nombre = nombre;
+	this.color = color;
+
+	this.nivelDeAmistad = 0;
+
+	this.vida = 100;
+	this.poderDeAtaque = poderDeAtaque;
+
+	this.mostrarPokemon = function(){
+		return ("Hola, soy: " + this.nombre + " y soy de color: " + this.color);
+	}
+
+	this.aumentarAmistad = function(valor){
+    	this.nivelDeAmistad = this.nivelDeAmistad + valor;
+    	return ("La nueva amistad de " + this.nombre + " es: " + this.nivelDeAmistad);
+	}
+
+	this.atacar = function(pokemon){
+    	pokemon.vida = (pokemon.vida - this.poderDeAtaque);
+    	return ("Este Pokémon perdió: " + pokemon.vida);
+	}
+}
+
+var Pikachu = new Pokemon ("Pikachu", "amarillo", 100)
+var Charmander = new Pokemon ("Charmander", "rojo", 20)
+
+
+var info = document.getElementById("caja");
+
+info.innerHTML += Pikachu.aumentarAmistad(30);
